4fdbe9b36d198cd7968fd25b954832134deb5dd3
kround <yuan40kang@126.com>
2017-03-09 16:50:41 +0800

    添加自动git任务

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,6 @@ var gulpSequence = require('gulp-sequence');
 require('./task/gulp.js');
 
 gulp.task('dev', gulpSequence('clean','webpack','replace','upload'));
+
+
+// gulp.task('dev', gulpSequence('clean','webpack','replace','open','init','add','commit','checkout','remote','push','upload'));
--- task/gulp.js
+++ task/gulp.js
@@ -4,6 +4,8 @@ const qiniu = require('gulp-qiniu');
 const replace = require('gulp-replace');
 const webpack = require('webpack');
 const clean = require('gulp-clean');
+const git = require('gulp-git');
+const exec = require('child_process').exec;
 
 const config = require('./config.json');
 const type = config[process.argv[2]] ? process.argv[2] : 'dev';
@@ -14,7 +16,7 @@ const WebpackConfig = require('./webpack.prod')(config[type], config.v);
 清除任务
 */
 gulp.task('clean', function () {
-    return gulp.src(['./dist/*.html','./dist/*.css', './dist/*.js','./dist/assets/**','./compiled'])
+    return gulp.src(['./dist/*', './compiled/*'])
         .pipe(clean());
 })
 
@@ -39,6 +41,74 @@ gulp.task('replace', function () {
         .pipe(replace('./assets/static/', WebpackConfig.output.publicPath + 'assets/static/'))
         .pipe(gulp.dest('./dist/'));
 })
+
+/*
+创建子进程打开子git目录
+*/
+gulp.task('open', function (callback) {
+    exec('cd dist', function (error, stdout, stderr) {
+        callback()
+    })
+})
+
+/*
+git初始化
+*/
+gulp.task('init', function (callback) {
+    exec('git init', function (error, stdout, stderr) {
+        callback()
+    })
+})
+
+
+/*
+git添加
+*/
+gulp.task('add', function (callback) {
+    exec('git add index.html', function (error, stdout, stderr) {
+        callback()
+    })
+});
+
+/*
+git添加远程仓库
+*/
+
+/*
+git提交
+*/
+gulp.task('commit', function (callback) {
+    exec('git commit -m '+'发布时间:'+new Date(), function (error, stdout, stderr) {
+        callback()
+    })
+});
+
+/*
+创建并切换分支
+*/
+gulp.task('checkout', function (callback) {
+    exec('git checkout -b '+config[type].branch, function (error, stdout, stderr) {
+        callback()
+    })
+});
+
+/*
+添加远程仓库
+*/
+gulp.task('remote', function (callback) {
+    exec('git remote add origin '+config[type].remote, function (error, stdout, stderr) {
+        callback()
+    })
+});
+
+/*
+git推送
+*/
+gulp.task('push', function (callback) {
+    exec('git push -u origin '+config[type].remote, function (error, stdout, stderr) {
+        callback()
+    })
+});
 
 /*
 上传七牛
